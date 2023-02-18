6c92db50d7f967899f1036018c4faba0fdc627ba
chlorophilia <andrewsimon@MacBook-Air.lan>
2023-02-17 18:25:36 -0800

    added loop to R Markdown doc that calls headers and text from 'test.csv' and applies formatting.
    Converted basemap raster from floating point to integer and updated map aesthetics

--- scripts/Howe_Sound.R
+++ scripts/Howe_Sound.R
@@ -7,6 +7,11 @@ source("scripts/utils.R")
 # Layer 1: Salish Sea DSM
 SS_DSM <- raster("spatial_data/rasters/SS_DEM_200x200.tif")
 
+# Convert floating point to integers
+dataType(SS_DSM)="INT4S"
+
+SS_DSM <- round(SS_DSM)
+
 # Layer 2: Salish Sea Region
 boundary <- mx_read("spatial_data/vectors/boundary")
 
@@ -16,18 +21,18 @@ islands <- mx_read("spatial_data/vectors/islands")
 # Layer 4: Location
 location <- mx_read("spatial_data/vectors/locations/Howe_Sound")
 
-#Render leaflet map
+# Create raster palette
 
-#Create palette
-# pal <- colorNumeric(
-# palette = "Blues",
-# domain = SS_DSM$values)
+pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(SS_DSM),
+                    na.color = "transparent")
+
+# Render leaflet map
 
 Map <- leaflet() %>%
   addTiles(options = providerTileOptions(opacity = 0.5)) %>%
-  addRasterImage(SS_DSM, opacity = 0.8) %>% 
+  addRasterImage(SS_DSM, colors =pal, opacity = 0.8) %>% 
   addPolygons(data = boundary, color = "blue", weight = 2, fillOpacity = 0) %>%
   addPolygons(data = islands, color = "blue", weight = 1, fillOpacity = 0) %>%
-  addPolygons(data = location, color = "red", weight = 2, fillOpacity = 0) %>%
+  addPolygons(data = location, color = "yellow", weight = 2, fillOpacity = 0) %>%
 
 print(Map)
